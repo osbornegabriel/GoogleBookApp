0057576e71178fe73efd276ae0d9efc6fd46b67f
Gabriel Osborne <osbornegabriel@gmail.com>
2019-01-27 17:24:40 -0500

    Write bookSearch and formatBookInfo functions for sending json request to google api and attaching formatted info to webpage

--- application.js
+++ application.js
@@ -5,6 +5,24 @@ $(document).ready(function(){
 var bookSearch = function(){
   $('#button').on('click', function(e){
     var search = $('#search').val();
-    console.log(search);
+    $.ajax({
+      method: 'GET',
+      url: "https://www.googleapis.com/books/v1/volumes?q=" + search,
+      datatype: "json"
+    }).done(function(response){
+      $('#results').text('');
+      for(i = 0; i < 10; i++){
+        var item = response.items[i].volumeInfo;
+        $('#results').append(formatBookInfo(item));
+      }    })
   })
+}
+
+var formatBookInfo = function(item){
+  var info = "<h3 class='title'><em>" + item.title + "</em></h3>";
+  info += "<p><strong>Author:</strong> " + item.authors;
+  info += "<br><strong>Publisher:</strong> " + item.publisher;
+  info += "<br><a target='_blank' href='" + item.infoLink + "'>More Info</a></p>";
+  info += "<img class='thumbnail' src='" + item.imageLinks.smallThumbnail + "'>"
+  return info;
 }
