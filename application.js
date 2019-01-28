bdcb2d6728d46f587069a3ce1bf914dae4d5af6b
Gabriel Osborne <osbornegabriel@gmail.com>
2019-01-27 20:26:42 -0500

    Add error catching for missing book info

--- application.js
+++ application.js
@@ -3,10 +3,20 @@ var showBookInfo;
 var formatBookInfo;
 var resetResults;
 var searchListeners;
+var apiCall;
+var infoCheck;
+var descriptionLines;
+var publisherLine;
+var bookInfoAndCover;
+var authorLine;
+var titleLine;
+var bookImage;
+
 
 $(document).ready(function(){
   searchListeners();
 })
+
 
 searchListeners = function(){
   $('#button').on('click', function(e){
@@ -21,13 +31,17 @@ searchListeners = function(){
 
 bookSearch = function(){
   var search = $('#search').val();
-  $.ajax({
+  apiCall(search).done(function(response){
+    resetResults();
+    showBookInfo(response);
+  })
+}
+
+apiCall = function(search){
+  return $.ajax({
     method: 'GET',
     url: "https://www.googleapis.com/books/v1/volumes?q=" + search,
     datatype: "json"
-  }).done(function(response){
-    resetResults();
-    showBookInfo(response);
   })
 }
 
@@ -39,17 +53,50 @@ showBookInfo = function(bookList){
   var itemInfo;
   for(i = 0; i < bookList.items.length; i++){
     itemInfo = bookList.items[i].volumeInfo;
-    $('#results').append(formatBookInfo(itemInfo));
+    bookBlurb = formatBookInfo(itemInfo);
+    $('#results').append(bookBlurb);
   }
 }
 
 formatBookInfo = function(item){
   var info = "<div class='book-info'>"
-  info += "<h3><span class='title'>" + item.title + "</span></h3>";
-  info += "<p><strong>Author:</strong> " + item.authors;
-  info += "<br><strong>Publisher:</strong> " + item.publisher;
-  info += "<br><a target='_blank' href='" + item.infoLink + "'>";
-  info += "<br><img class='thumbnail' src='" + item.imageLinks.smallThumbnail + "'></a>";
-  info += "</p></div>";
+  info += titleLine(item);
+  info += descriptionLines(item);
+  info += "</div>";
   return info;
+}
+
+infoCheck = function(info){
+  return info ? info : "Not Available";
+}
+
+descriptionLines = function(book){
+  var description = new String();
+  description += authorLine(book);
+  description += publisherLine(book);
+  description += bookInfoAndCover(book);
+  return "<p>" + description + "</p>";
+}
+
+publisherLine = function(book){
+  return "<br><strong>Publisher:</strong> " + infoCheck(book.publisher);
+}
+
+bookInfoAndCover = function(book){
+  return "<br><br><a target='_blank' href='" + book.infoLink + "'>" + bookImage(book) + "</a>";
+}
+
+authorLine = function(book){
+  return "<strong>Author:</strong> " + infoCheck(book.authors);
+}
+
+titleLine = function(book){
+  var title = infoCheck(book.title);
+  return "<h3><span class='title'>" + title + "</span></h3>"
+}
+
+bookImage = function(book){
+  var link = book.imageLinks;
+  var picture = link ? link.smallThumbnail : false;
+  return picture ? "<img class='thumbnail' src='" + picture + "'>" : "Click for Info";
 }
